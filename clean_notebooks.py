046bc4eff04d0b4bad4806756f69a5eed422699f
Philip R. Jarnhus <pja@danskebank.dk>
2018-06-25 13:42:59 +0200

    Created script for cleaning notebooks of metadata and output data. This was done to ease the merge conflict resolutions

---
+++ clean_notebooks.py
@@ -0,0 +1,45 @@
+import json
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Clean-up exercise notebooks.')
+parser.add_argument('filenames', metavar='filename', type=str, nargs='+',
+                   help='names of the notebooks to be cleaned')
+
+args = parser.parse_args()
+
+# Open existing notebook
+for filename in ([('',fn)
+                    for fn in args.filenames
+                    if fn[-6:] == '.ipynb']
+                +[('Solutions/',fn)
+                    for fn in args.filenames
+                    if fn[-6:] == '.ipynb']):
+    with open(filename[0]+filename[1]) as f:
+        nb = json.load(f)
+        f.close()
+    
+    if nb['nbformat'] != 4:
+        continue
+    
+    # Create a new dictinary with only the clean data
+    nb_clean = dict()
+    
+    # Transfer version number
+    nb_clean['nbformat'] = nb['nbformat']
+    nb_clean['nbformat_minor'] = nb['nbformat_minor']
+    
+    # Only copy relevant metadata
+    nb_clean['metadata'] = {k: v for k,v in nb['metadata'].items()
+                                if k in ['kernelspec', 'authors']}
+    
+
+    # Clean up notebook cells
+    for cell in nb['cells']:
+        cell['metadata'] = {}
+        if cell['cell_type'] == 'code' and filename[0] != 'Solutions/':
+            cell['outputs'] = []
+            cell['execution_count'] = None
+    
+    nb_clean['cells'] = nb['cells']
+    json.dump(nb_clean, open(filename[0]+filename[1], 'w'))
